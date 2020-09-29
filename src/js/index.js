72a0b1d71181dd5afbc16cc3b6d9a66e364d3f5b
SohanR <mizan.rahman66d@gmail.com>
2020-09-29 18:27:30 +0600

    load models

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,19 @@
 
 const video = document.getElementById("video");
 
-function startVideo() {}
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUrl("./models"),
+  faceapi.nets.faceLandmar68Net.loadFromUrl("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUrl("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUrl("./models"),
+]).then(startVideo);
+
+//get access to webcam and stream video to browser
+function startVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: {} })
+    .then((stream) => (video.srcObject = stream))
+    .catch((err) => console.error(err));
+}
+
+startVideo();
